c1651d47a672314cc56254c6ea4ac7a11efe1b4b
alipoorseyed <poorseyedali40@gmail.com>
2024-09-17 00:59:25 +0330

    update card-homepage

--- src/components/Card_Homepage.tsx
+++ src/components/Card_Homepage.tsx
@@ -6,7 +6,7 @@ interface CardHomepageProps {
 
 export const Card_Homepage: React.FC<CardHomepageProps> = ({ title }) => {
   return (
-    <div className="bg-slate-700 w-18 h-18 text-white p-4 rounded">
+    <div className="bg-slate-700 w-[50px] h-[50px] text-white p-1 rounded">
       {title}
     </div>
   );
