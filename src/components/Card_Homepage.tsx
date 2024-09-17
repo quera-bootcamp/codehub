d57e4411a7da5fe3ae776844ad59ad65d97f6a11
alipoorseyed <poorseyedali40@gmail.com>
2024-09-17 17:51:12 +0330

    update and compelet Like_Icon.tsx , Card_Homepage.tsx

--- src/components/Card_Homepage.tsx
+++ src/components/Card_Homepage.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Like_Icon } from './Like_Icon';
+import { Badge } from './Badge';
 
 interface CardHomepageProps {
   title: string;
+  imgsrce : string;
 }
 
-export const Card_Homepage: React.FC<CardHomepageProps> = ({ title }) => {
+export const Card_Homepage: React.FC<CardHomepageProps> = ({ title , imgsrce = ""}) => {
   return (
-    <div className="bg-slate-700 w-[50px] h-[50px] text-white p-1 rounded">
-      {title}
+    <div className="flex flex-col gap-3 justify-start items-stretch w-[300px] relative">
+      <Like_Icon/>
+      <img className='rounded-xl' src={imgsrce} alt="" />
+      <div className="flex flex-row justify-between items-center">
+        <p className='text-[14px] font-[400] text-[black]'>{title}</p>
+        <Badge price={"123000"}/>
+      </div>
     </div>
   );
 };
--- src/components/Like_Icon.tsx
+++ src/components/Like_Icon.tsx
@@ -5,6 +5,6 @@ import Icon_red from "../assets/Like-Icon-Red.svg"
 export const Like_Icon = () => {
     const [like,setlike] = useState<boolean>(false);
   return (
-    <img onClick={() => setlike(!like)} src={like ? Icon_red : Icon} className="cursor-pointer" alt="Like" />
+    <img onClick={() => setlike(!like)} src={like ? Icon_red : Icon} className="cursor-pointer absolute top-2 right-2" alt="Like" />
   )
 }
