d2e75d00f9252dabc56a1c6fbcc895afa6870e3f
alipoorseyed <poorseyedali40@gmail.com>
2024-09-17 23:56:05 +0330

    fixing config problem

--- src/routes/Testing.tsx
+++ src/routes/Testing.tsx
@@ -1,7 +1,10 @@
+import { Card_Homepage } from "../components/Card_Homepage"
+import Icon from "../assets/photo_6003374028309514589_y300.jpg";
 export default function Testing() {
     return (
         <div className={'font-primaryBold text-2xl'}>
             <h1>page for testing your codes</h1>
+            <Card_Homepage title="iphone" imgsrce={Icon} />
         </div>
     )
 }
