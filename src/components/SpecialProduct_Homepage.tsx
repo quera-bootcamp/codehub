df2f76a8954909b021cb24e3990457d5b0721eda
alipoorseyed <poorseyedali40@gmail.com>
2024-09-21 21:34:55 +0330

    update style of SpecialProduct_HomePage.tsx

--- src/components/SpecialProduct_Homepage.tsx
+++ src/components/SpecialProduct_Homepage.tsx
@@ -8,7 +8,7 @@ export const SpecialProduct_Homepage = () => {
     <div className='w-full'>
         <div className='w-full m-6 flex flex-row justify-between items-center'>
             <h2 className='text-4xl font-normal text-default-950 dark:text-default-100'>محصولات ویژه</h2>
-            <Button value='فروشگاه' button_style='text-default-100 cursor-pointer' />
+            <Button value='فروشگاه' button_style='text-default-100 cursor-pointer px-8 py-2 rounded-xl' />
         </div>
         <Homepage_Card_Container style_for_card='w-56' style='w-full' items={iteems} />
     </div>
