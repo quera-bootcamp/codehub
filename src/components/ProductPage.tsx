5e959ab44c180e2220657bcd2e822cdd799f7ba7
nima <nimazare1378@gmail.com>
2024-09-21 22:22:29 +0330

    mackbookpro image has been add to public/images/ & productpage and testing hass been modified

---
+++ src/components/ProductPage.tsx
@@ -0,0 +1,32 @@
+const ProductPage = () => {
+  return (
+    <div className="flex gap-4 p-11 bg-slate-200">
+      <img className="rounded-lg" src="./images/mackbookPro.jpg" alt="" />
+
+      <div className="">
+        <h1 className="font-bold">Apple MacBook Air M2</h1>
+        <h5>
+          مک بوک ایر با تراشه M2 دارای صفحه نمایش 13.6 اینچی رتینا است. تا 18
+          ساعت عمر باتری و طراحی بدون فن.
+        </h5>
+        <p>۱۰,۰۰۰ تومان</p>
+        <div>
+          <div>
+            <p></p>
+            <p></p>
+            <p></p>
+          </div>
+          <div>
+            <p></p>
+            <p></p>
+            <p></p>
+          </div>
+        </div>
+        <div>
+          <p>نظر</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default ProductPage;
--- src/routes/Testing.tsx
+++ src/routes/Testing.tsx
@@ -1,7 +1,5 @@
+import ProductPage from "../components/ProductPage";
+
 export default function Testing() {
-    return (
-        <div className={'font-primaryBold text-2xl'}>
-            <h1>page for testing your codes</h1>
-        </div>
-    )
+  return <ProductPage />;
 }
