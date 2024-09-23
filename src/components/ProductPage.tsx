334a717a0540b8f8b96504d599dd8e436161ac5d
nima <nimazare1378@gmail.com>
2024-09-23 18:41:06 +0330

    feat[productPage]: implemented 35% of product page, productPage.tsx has been modified

--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -1,15 +1,15 @@
 const ProductPage = () => {
   return (
-    <div className="flex gap-4 p-11 bg-slate-200">
+    <div className="flex gap-x-32 py-11">
       <img className="rounded-lg" src="./images/mackbookPro.jpg" alt="" />
 
-      <div className="">
-        <h1 className="font-bold">Apple MacBook Air M2</h1>
-        <h5>
+      <div className="flex flex-col gap-y-20">
+        <h1 className="font-bold text-2xl">Apple MacBook Air M2</h1>
+        <h5 className="text-lg">
           مک بوک ایر با تراشه M2 دارای صفحه نمایش 13.6 اینچی رتینا است. تا 18
           ساعت عمر باتری و طراحی بدون فن.
         </h5>
-        <p>۱۰,۰۰۰ تومان</p>
+        <p className="text-5xl">۱۰,۰۰۰ تومان</p>
         <div>
           <div>
             <p></p>
@@ -22,8 +22,142 @@ const ProductPage = () => {
             <p></p>
           </div>
         </div>
-        <div>
-          <p>نظر</p>
+        <div className="flex justify-between">
+          <div className="flex gap-2">
+            <p>۵۰۰۰ نظر</p>
+            <div className="flex">
+              <svg
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g clipPath="url(#clip0_37_750)">
+                  <path
+                    d="M15.1805 6.17781L10.8112 5.54169L8.85869 1.58975C8.68317 1.23661 8.34078 1.05811 7.9984 1.05811C7.6581 1.05811 7.3181 1.23422 7.14198 1.58975L5.18884 5.54139L0.819292 6.17691C0.0357097 6.29034 -0.27832 7.25422 0.289739 7.80646L3.45063 10.8811L2.70257 15.2238C2.59631 15.8441 3.09153 16.3417 3.64616 16.3417C3.79332 16.3417 3.94466 16.3068 4.09004 16.2297L7.99899 14.1796L11.9076 16.2303C12.0527 16.3065 12.2038 16.3411 12.3503 16.3411C12.9056 16.3411 13.4017 15.8453 13.2954 15.2247L12.5482 10.8817L15.7097 7.80766C16.2781 7.25542 15.9641 6.29124 15.1805 6.17781ZM11.5465 9.85542L11.0056 10.3814L11.1333 11.1241L11.716 14.5106L8.66735 12.9109L7.99929 12.5605L8.00019 3.09214L9.52347 6.17542L9.8572 6.85094L10.6038 6.9596L14.0136 7.45602L11.5465 9.85542Z"
+                    fill="black"
+                  />
+                </g>
+                <defs>
+                  <clipPath id="clip0_37_750">
+                    <rect
+                      width="16"
+                      height="16"
+                      fill="white"
+                      transform="translate(0 0.699951)"
+                    />
+                  </clipPath>
+                </defs>
+              </svg>
+              <svg
+                width="16"
+                height="17"
+                viewBox="0 0 16 17"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g clipPath="url(#clip0_37_747)">
+                  <path
+                    d="M7.20268 2.08318L5.38879 5.76095L1.33046 6.35262C0.602682 6.45818 0.311016 7.3554 0.838794 7.86929L3.7749 10.7304L3.08046 14.7721C2.95546 15.5026 3.7249 16.0498 4.36935 15.7082L7.99991 13.7998L11.6305 15.7082C12.2749 16.0471 13.0443 15.5026 12.9193 14.7721L12.2249 10.7304L15.161 7.86929C15.6888 7.3554 15.3971 6.45818 14.6693 6.35262L10.611 5.76095L8.79713 2.08318C8.47213 1.42762 7.53046 1.41929 7.20268 2.08318Z"
+                    fill="black"
+                  />
+                </g>
+                <defs>
+                  <clipPath id="clip0_37_747">
+                    <rect
+                      width="16"
+                      height="16"
+                      fill="white"
+                      transform="translate(0 0.699951)"
+                    />
+                  </clipPath>
+                </defs>
+              </svg>
+              <svg
+                width="16"
+                height="17"
+                viewBox="0 0 16 17"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g clipPath="url(#clip0_37_747)">
+                  <path
+                    d="M7.20268 2.08318L5.38879 5.76095L1.33046 6.35262C0.602682 6.45818 0.311016 7.3554 0.838794 7.86929L3.7749 10.7304L3.08046 14.7721C2.95546 15.5026 3.7249 16.0498 4.36935 15.7082L7.99991 13.7998L11.6305 15.7082C12.2749 16.0471 13.0443 15.5026 12.9193 14.7721L12.2249 10.7304L15.161 7.86929C15.6888 7.3554 15.3971 6.45818 14.6693 6.35262L10.611 5.76095L8.79713 2.08318C8.47213 1.42762 7.53046 1.41929 7.20268 2.08318Z"
+                    fill="black"
+                  />
+                </g>
+                <defs>
+                  <clipPath id="clip0_37_747">
+                    <rect
+                      width="16"
+                      height="16"
+                      fill="white"
+                      transform="translate(0 0.699951)"
+                    />
+                  </clipPath>
+                </defs>
+              </svg>
+              <svg
+                width="16"
+                height="17"
+                viewBox="0 0 16 17"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g clipPath="url(#clip0_37_747)">
+                  <path
+                    d="M7.20268 2.08318L5.38879 5.76095L1.33046 6.35262C0.602682 6.45818 0.311016 7.3554 0.838794 7.86929L3.7749 10.7304L3.08046 14.7721C2.95546 15.5026 3.7249 16.0498 4.36935 15.7082L7.99991 13.7998L11.6305 15.7082C12.2749 16.0471 13.0443 15.5026 12.9193 14.7721L12.2249 10.7304L15.161 7.86929C15.6888 7.3554 15.3971 6.45818 14.6693 6.35262L10.611 5.76095L8.79713 2.08318C8.47213 1.42762 7.53046 1.41929 7.20268 2.08318Z"
+                    fill="black"
+                  />
+                </g>
+                <defs>
+                  <clipPath id="clip0_37_747">
+                    <rect
+                      width="16"
+                      height="16"
+                      fill="white"
+                      transform="translate(0 0.699951)"
+                    />
+                  </clipPath>
+                </defs>
+              </svg>
+              <svg
+                width="16"
+                height="17"
+                viewBox="0 0 16 17"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g clipPath="url(#clip0_37_747)">
+                  <path
+                    d="M7.20268 2.08318L5.38879 5.76095L1.33046 6.35262C0.602682 6.45818 0.311016 7.3554 0.838794 7.86929L3.7749 10.7304L3.08046 14.7721C2.95546 15.5026 3.7249 16.0498 4.36935 15.7082L7.99991 13.7998L11.6305 15.7082C12.2749 16.0471 13.0443 15.5026 12.9193 14.7721L12.2249 10.7304L15.161 7.86929C15.6888 7.3554 15.3971 6.45818 14.6693 6.35262L10.611 5.76095L8.79713 2.08318C8.47213 1.42762 7.53046 1.41929 7.20268 2.08318Z"
+                    fill="black"
+                  />
+                </g>
+                <defs>
+                  <clipPath id="clip0_37_747">
+                    <rect
+                      width="16"
+                      height="16"
+                      fill="white"
+                      transform="translate(0 0.699951)"
+                    />
+                  </clipPath>
+                </defs>
+              </svg>
+            </div>
+          </div>
+          <form action="">
+            <label htmlFor="nums"></label>
+            <select name="nums" id="nums">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </form>
         </div>
       </div>
     </div>
