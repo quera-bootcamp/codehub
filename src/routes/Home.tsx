01b4268b65d8e237e60c89b179515f77a38f8aca
nima <nimazare1378@gmail.com>
2024-09-28 19:25:17 +0330

    fix[Home]: home.tsx modified some lines because of editing slider component and it does not need to pass activeItem any more as a prop

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,27 +1,18 @@
-import {useState} from "react";
 import Slider from "../components/Slider";
-import sliderData from "../../mock-data/SliderItems";
-import Button2 from "../components/button2.tsx";
+import productInfo from "../../mock-data/ProductInfo.tsx";
 import ProductCard from "../components/ProductCard.tsx";
 
 const Home = () => {
-    const [activeItemIndex, setActiveItemIndex] = useState(0);
-    return (
-        <>
-            <div className="flex justify-between">
-                {/*TO DO: Card home page*/}
-                <div>
-                <ProductCard />
-                </div>
-                <Slider
-                    activeItemIndex={activeItemIndex}
-                    setActiveItemIndex={setActiveItemIndex}
-                    sliderData={sliderData}
-                />
-
-            </div>
-            {/* مجصولات ویژه */}
-        </>
-    );
+  return (
+    <>
+      <div className="flex justify-between">
+        {/*TO DO: Card home page*/}
+        <div>
+          <ProductCard />
+        </div>
+        <Slider sliderData={productInfo} />
+      </div>
+    </>
+  );
 };
 export default Home;
