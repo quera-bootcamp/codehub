e3763e51aec86d4cb9d524f2f38d991b0d5fb63a
nima <nimazare1378@gmail.com>
2024-09-28 19:27:31 +0330

    fix[mock-data]: SliderItem renamed to ProductInfo

---
+++ mock-data/ProductInfo.tsx
@@ -0,0 +1,60 @@
+const productInfo = [
+  {
+    id: 1,
+    imgSrc: "./images/iphone-14-pro-model-unselect-gallery-1-202209.png",
+    price: "9,000",
+    brand: "Apple",
+    time: "چند روز پیش",
+    rating: 4.5,
+    quantity: 57,
+    stock: 20,
+    productName: "Apple Iphone 16",
+    comments: 5,
+    description:
+      "آیفون 14 دارای صفحه نمایش 6.1 اینچی  Retina XDR است. تراشه A18 Bionic و سیستم دوربین دو گانه.",
+  },
+  {
+    id: 2,
+    imgSrc: "./images/iphone1.png",
+    price: "5,000",
+    brand: "Apple",
+    time: "چند لحظه پیش",
+    rating: 3,
+    quantity: 22,
+    stock: 10,
+    productName: "Apple Iphone 16",
+    comments: 10,
+    description:
+      "آیفون 15 دارای صفحه نمایش 6.1 اینچی Super Retina XDR است. تراشه A18 Bionic و سیستم دوربین دو گانه.",
+  },
+  {
+    id: 3,
+    imgSrc: "./images/iphone15.png",
+    price: "85,000",
+    brand: "Apple",
+    time: "چند لحظه پیش",
+    rating: 5,
+    quantity: 52,
+    stock: 10,
+    productName: "Apple Iphone 16",
+    comments: 10,
+    description:
+      "آیفون 16 دارای صفحه نمایش 6.1 اینچی Super Retina XDR است. تراشه A18 Bionic و سیستم دوربین دو گانه.",
+  },
+  {
+    id: 4,
+    imgSrc: "./images/iphone2.png",
+    price: "10,000",
+    brand: "Apple",
+    time: "همین الان",
+    rating: 5,
+    quantity: 62,
+    stock: 10,
+    productName: "Apple Iphone 16",
+    comments: 10,
+    description:
+      "آیفون 10 دارای صفحه نمایش 6.1 اینچی Super Retina XDR است. تراشه A18 Bionic و سیستم دوربین دو گانه.",
+  },
+];
+
+export default productInfo;
