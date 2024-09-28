2ba271e66a41cdbde782c36420f3c37ee02dd589
A.Rezaei <rezaei.arshia1382@gmail.com>
2024-09-28 13:26:11 +0330

    feat[shops]: complete shops page, add ShopCards component, add ShopFilter component

---
+++ mock-data/ShopPageItems.ts
@@ -0,0 +1,52 @@
+import shopImage from "../public/images/shopImage.png"
+
+export const shopItem = [
+    {
+        id: 1,
+        image: shopImage,
+        alt: 'image',
+        title: 'Apple iPhone 14 Pro',
+        price: '۱۰,۰۰۰',
+        description: 'آیفون 14 پرو دارای صفحه نمایش 6.1 اینچی سوپر است نمایشگر Retina XDR آیفون 14 پرو دارای صفحه نمایش 6.1 اینچی سوپر است نمایشگر',
+    },
+    {
+        id: 2,
+        image: shopImage,
+        alt: 'image',
+        title: 'Apple iPhone 14 Pro',
+        price: '۱۰,۰۰۰',
+        description: 'آیفون 14 پرو دارای صفحه نمایش 6.1 اینچی سوپر است نمایشگر Retina XDR آیفون 14 پرو دارای صفحه نمایش 6.1 اینچی سوپر است نمایشگر',
+    },
+    {
+        id: 3,
+        image: shopImage,
+        alt: 'image',
+        title: 'Apple iPhone 14 Pro',
+        price: '۱۰,۰۰۰',
+        description: 'آیفون 14 پرو دارای صفحه نمایش 6.1 اینچی سوپر است نمایشگر Retina XDR آیفون 14 پرو دارای صفحه نمایش 6.1 اینچی سوپر است نمایشگر',
+    },
+    {
+        id: 4,
+        image: shopImage,
+        alt: 'image',
+        title: 'Apple iPhone 14 Pro',
+        price: '۱۰,۰۰۰',
+        description: 'آیفون 14 پرو دارای صفحه نمایش 6.1 اینچی سوپر است نمایشگر Retina XDR آیفون 14 پرو دارای صفحه نمایش 6.1 اینچی سوپر است نمایشگر',
+    },
+    {
+        id: 5,
+        image: shopImage,
+        alt: 'image',
+        title: 'Apple iPhone 14 Pro',
+        price: '۱۰,۰۰۰',
+        description: 'آیفون 14 پرو دارای صفحه نمایش 6.1 اینچی سوپر است نمایشگر Retina XDR آیفون 14 پرو دارای صفحه نمایش 6.1 اینچی سوپر است نمایشگر',
+    },
+    {
+        id: 6,
+        image: shopImage,
+        alt: 'image',
+        title: 'Apple iPhone 14 Pro',
+        price: '۱۰,۰۰۰',
+        description: 'آیفون 14 پرو دارای صفحه نمایش 6.1 اینچی سوپر است نمایشگر Retina XDR آیفون 14 پرو دارای صفحه نمایش 6.1 اینچی سوپر است نمایشگر',
+    },
+]
--- src/routes/Testing.tsx
+++
@@ -1,7 +0,0 @@
-export default function Testing() {
-    return (
-        <div className={'font-primaryBold text-2xl'}>
-            <h1>page for testing your codes</h1>
-        </div>
-    )
-}
