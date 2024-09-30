c3771a323307552d7c8e2f2b5efc944ff8ab126a
A.Rezaei <rezaei.arshia1382@gmail.com>
2024-09-30 17:15:06 +0330

    feat[homepage-welcome]: refactor homepage user data , fix NavbarModalItems component id's

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,16 +1,48 @@
+import {Link} from "react-router-dom";
 import Slider from "../components/Slider";
+import {useTheme} from "../stroe/ThemeContext.tsx";
+import {useUser} from "../userStore/UserContext.tsx";
 import productInfo from "../../mock-data/ProductInfo";
+import codeHubDark from "../../public/images/codeHubDark.png";
+import codeHubLight from "../../public/images/codeHubLight.png";
 
 const Home = () => {
-  return (
-    <>
-      <div className="flex justify-between">
-        {/*TO DO: Card home page*/}
-        <div></div>
-        <Slider sliderData={productInfo} />
-      </div>
-      {/* مجصولات ویژه */}
-    </>
-  );
+    const {user} = useUser();
+    const {darkMode} = useTheme();
+    return (
+        <>
+            <div className="flex justify-between">
+                <div>
+                    <img
+                        src={darkMode ? codeHubDark : codeHubLight}
+                        alt="logo"
+                        className={'w-60 mx-auto'}
+                    />
+                    {user ?
+                        <>
+                            <p><span className={'text-2xl'}>{user.email}</span> به<span
+                                className={'font-primaryBold text-xl'}> کد<span
+                                className={'text-orange-500'}>هاب</span></span> خوش امدید</p>
+                            <p className={'pt-8'}>اوقات خوشی را برای شما <span className={'font-primaryBold text-xl'}> کد<span
+                                className={'text-orange-500'}>هابی</span></span> عزیز آرزومندیم</p>
+                        </>
+                        :
+                        <>
+                            <p className={'-mt-8'}>
+                                لذت استفاده از<span className={'font-primaryBold text-xl'}> کد<span
+                                className={'text-orange-500'}>هاب</span></span> را
+                                با
+                                <Link className={'text-danger font-primaryBold text-xl px-2'} to={'/Register'}> ثبت
+                                    نام </Link>
+                                رایگان تجربه کنید
+                            </p>
+                        </>
+                    }
+                </div>
+                <Slider sliderData={productInfo}/>
+            </div>
+            {/* مجصولات ویژه */}
+        </>
+    );
 };
 export default Home;
