e918a2b6d08c9eb1930f66ca16a17d85e4554bfb
alipoorseyed <poorseyedali40@gmail.com>
2024-10-07 13:04:58 +0330

    fix[update the functionality of LikeIcon.tsx and Favorit.txs]

--- src/routes/Favorites.tsx
+++ src/routes/Favorites.tsx
@@ -1,9 +1,22 @@
+import { CardHomepage } from "../components/CardHomepage";
+import {useFavorites} from "../hooks/FavoritesContext"
 const Favorites = () => {
-    return (
-        <div>
-            <p>this is favorites page</p>
+    const { favorites } = useFavorites();
+    if(favorites.length){
+      return (
+        <div className="flex flex-row w-full gap-7 flex-wrap justify-start items-start">
+             {
+               favorites.map((element)  => (
+                <CardHomepage initialState={true} style="w-56" id={element.id} imgsrc={element.imgsrc} title={element.title} price={element.price} />
+               ))
+             }
         </div>
     );
+    }else{
+      return(
+        <div className="h-screen flex justify-center items-center text-default-950 font-bold text-xl">شما کالای مورد علاقه ای ندارید . </div>
+      );
+    }
 };
 
 export default Favorites;
